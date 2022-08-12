c1cbd1a428f4560ecfd26036f79feae072393a46
BHARATH RAJ <BHARATH RAJ@DESKTOP-146C0CD>
2022-08-12 13:15:05 +0530

    OK

--- Actitime/src/main/java/genericLib/Basetest.java
+++ Actitime/src/main/java/genericLib/Basetest.java
@@ -1,19 +1,17 @@
 package genericLib;
-
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-public class Basetest {
+public class Basetest
+{
 	public WebDriver driver;
-
-	@BeforeClass
-	public void browserlaunch() throws IOException {
+    @BeforeClass
+	    public void browserlaunch() throws IOException {
 		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
 		driver = new ChromeDriver();
 		Reporter.log("Browser is launched successfully");
--- Actitime/src/test/java/Testcase1/Actitime_1Test.java
+++ Actitime/src/test/java/Testcase1/Actitime_1Test.java
@@ -1,13 +1,11 @@
 package Testcase1;
-
 import org.testng.annotations.Test;
-
 import genericLib.Basetest;
-
 public class Actitime_1Test  extends Basetest{
 
-	@Test(groups = {"Smoke"})
-	public void logoff() {
+  @Test(groups = {"Smoke"})
+	
+  public void logoff() {
 		driver.get("https://www.pepsi.com");
 		driver.get("https://www.redbus.com");
 		driver.get("https://www.bingo.com");
